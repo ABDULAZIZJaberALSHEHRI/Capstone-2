7e732a5994b87ef44ab39326e33774dc4b24e21e
Abdulziz Jaber Alshehri <85577844+ABDULAZIZJaberALSHEHRI@users.noreply.github.com>
2024-08-19 02:33:41 +0300

    Add files via upload

---
+++ Capstone2/src/main/java/com/example/capstone2/Repository/Teacher_BalanceRepository.java
@@ -0,0 +1,13 @@
+package com.example.capstone2.Repository;
+
+import com.example.capstone2.Model.Teacher_Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Teacher_BalanceRepository extends JpaRepository<Teacher_Balance, Integer> {
+
+    @Query("select tb from Teacher_Balance tb where tb.teacher_id=?1")
+    Teacher_Balance findByTeacherId(int teacher_id);
+}
