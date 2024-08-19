9ef2f3529719bd37f143766a32f506104b46793c
Abdulziz Jaber Alshehri <85577844+ABDULAZIZJaberALSHEHRI@users.noreply.github.com>
2024-08-19 11:34:18 +0300

    Update Teacher_BalanceRepository.java

--- Capstone2/src/main/java/com/example/capstone2/Repository/Teacher_BalanceRepository.java
+++ Capstone2/src/main/java/com/example/capstone2/Repository/Teacher_BalanceRepository.java
@@ -10,4 +10,7 @@ public interface Teacher_BalanceRepository extends JpaRepository<Teacher_Balance
 
     @Query("select tb from Teacher_Balance tb where tb.teacher_id=?1")
     Teacher_Balance findByTeacherId(int teacher_id);
+
+    @Query("select t from Teacher_Balance t where t.teacher_id=?1")
+    List<Teacher_Balance> findBalanceByTeacherId(int teacher_id);
 }
